3353e76fbdf356a63495c3da248fb3222ac98c6c
RobertCavero <robertcavero404@gmail.com>
2024-09-15 01:27:07 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace memory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.lblTempo = new System.Windows.Forms.Label();
+            this.lblTentativas = new System.Windows.Forms.Label();
+            this.txtPontos = new System.Windows.Forms.TextBox();
+            this.btnModoNormal = new System.Windows.Forms.Button();
+            this.btnModoDificil = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(492, 65);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnReiniciar.TabIndex = 1;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTempo
+            // 
+            this.lblTempo.AutoSize = true;
+            this.lblTempo.Location = new System.Drawing.Point(492, 123);
+            this.lblTempo.Name = "lblTempo";
+            this.lblTempo.Size = new System.Drawing.Size(128, 16);
+            this.lblTempo.TabIndex = 1;
+            this.lblTempo.Text = "Tempo Restante: 30";
+            this.lblTempo.Click += new System.EventHandler(this.lblTempo_Click);
+            // 
+            // lblTentativas
+            // 
+            this.lblTentativas.AutoSize = true;
+            this.lblTentativas.Location = new System.Drawing.Point(492, 161);
+            this.lblTentativas.Name = "lblTentativas";
+            this.lblTentativas.Size = new System.Drawing.Size(73, 16);
+            this.lblTentativas.TabIndex = 2;
+            this.lblTentativas.Text = "Tentativas:";
+            this.lblTentativas.Click += new System.EventHandler(this.lblPontos_Click);
+            // 
+            // txtPontos
+            // 
+            this.txtPontos.Location = new System.Drawing.Point(573, 161);
+            this.txtPontos.Name = "txtPontos";
+            this.txtPontos.ReadOnly = true;
+            this.txtPontos.Size = new System.Drawing.Size(100, 22);
+            this.txtPontos.TabIndex = 3;
+            this.txtPontos.TextChanged += new System.EventHandler(this.txtPontos_TextChanged);
+            // 
+            // btnModoNormal
+            // 
+            this.btnModoNormal.Location = new System.Drawing.Point(513, 241);
+            this.btnModoNormal.Name = "btnModoNormal";
+            this.btnModoNormal.Size = new System.Drawing.Size(114, 23);
+            this.btnModoNormal.TabIndex = 1;
+            this.btnModoNormal.Text = "Modo Normal";
+            this.btnModoNormal.UseVisualStyleBackColor = true;
+            this.btnModoNormal.Click += new System.EventHandler(this.btnModoNormal_Click);
+            // 
+            // btnModoDificil
+            // 
+            this.btnModoDificil.Location = new System.Drawing.Point(513, 284);
+            this.btnModoDificil.Name = "btnModoDificil";
+            this.btnModoDificil.Size = new System.Drawing.Size(114, 23);
+            this.btnModoDificil.TabIndex = 1;
+            this.btnModoDificil.Text = "Modo Dificil";
+            this.btnModoDificil.UseVisualStyleBackColor = true;
+            this.btnModoDificil.Click += new System.EventHandler(this.btnModoDificil_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.lblTempo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnModoDificil);
+            this.Controls.Add(this.btnModoNormal);
+            this.Controls.Add(this.txtPontos);
+            this.Controls.Add(this.lblTentativas);
+            this.Controls.Add(this.lblTempo);
+            this.Controls.Add(this.btnReiniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Label lblTempo;
+        private System.Windows.Forms.Label lblTentativas;
+        private System.Windows.Forms.TextBox txtPontos;
+        private System.Windows.Forms.Button btnModoNormal;
+        private System.Windows.Forms.Button btnModoDificil;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memory
+{
+    public partial class Form1 : Form
+    {
+        enum ModoJogo
+        {
+            Normal,
+            Dificil
+        }
+
+        ModoJogo modoAtual = ModoJogo.Normal;
+        int[,] jogoNormal;
+        int[,] jogoDificil;
+        PictureBox c1;
+        PictureBox c2;
+        int iguais;
+        int tentativas;
+        int tempoTotal = 30;
+        int contagemRegressiva;
+        bool gameover = false;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            IniciarJogoNormal();
+        }
+        private void LimparPictureBoxes()
+        {
+            //remove as picture box para poder colocar as novas após embaralhar
+            foreach (Control control in Controls.OfType<PictureBox>().ToList())
+            {
+                Controls.Remove(control);
+                control.Dispose();
+            }
+        }
+
+        private void IniciarJogoNormal()
+        {
+            //limpa o espaço ocupado pelas cartas anteriores
+            LimparPictureBoxes();
+            SoundPlayer player = new SoundPlayer("audio/tema.wav");
+            player.Play();
+
+            int[] cartas = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+            Misturar(cartas);
+            jogoNormal = new int[4, 3];
+            int tamanhoCarta = 50;
+            int espaçamento = 60;
+            int margemEsquerda = 20;
+            int margemSuperior = 20;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.BackgroundImage = Image.FromFile($"cartas/bloco.png");
+                    pictureBox.BackgroundImageLayout = ImageLayout.Stretch;
+                    pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox.Width = pictureBox.Height = tamanhoCarta;
+                    pictureBox.Left = margemEsquerda + j * espaçamento;
+                    pictureBox.Top = margemSuperior + i * espaçamento;
+                    pictureBox.Click += PictureBox_Click;
+                    pictureBox.Tag = cartas[i * 3 + j];
+                    Controls.Add(pictureBox);
+                    jogoNormal[i, j] = cartas[i * 3 + j];
+                }
+            }
+
+            contagemRegressiva = tempoTotal;
+            timer1.Start();
+
+        }
+
+        
+
+
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            if (gameover)
+                return;
+
+            PictureBox pictureBox = sender as PictureBox;
+            int valor = (int)pictureBox.Tag;
+
+            //checa quais cartas foram viradas
+            if (pictureBox.Image != null || (c1 != null && c1 == pictureBox))
+                return;
+
+            //checa se a primeira carta foi selecionada
+            if (c1 == null)
+            {
+                c1 = pictureBox;
+                c1.Image = Image.FromFile($"cartas/{valor}.png");
+                c1.BackgroundImage = null;
+            }
+            else if (c2 == null) //checa se a segunda carta foi selecionada
+            {
+                c2 = pictureBox;
+                c2.Image = Image.FromFile($"cartas/{valor}.png");
+                c2.BackgroundImage = null;
+
+                //checa se as cartas são iguais
+                if ((int)c1.Tag == (int)c2.Tag)
+                {
+                    c1 = c2 = null;
+                    iguais++;
+                    if (iguais == 6 && modoAtual == ModoJogo.Normal)
+                    {
+                        Fim("Você ganhou!");
+                    }
+                    else if (iguais == 8 && modoAtual == ModoJogo.Dificil)
+                    {
+                        Fim("Você ganhou!");
+                    }
+                }
+                else
+                {
+                    tentativas++;
+                    txtPontos.Text = $"{tentativas}";
+                    Task.Delay(100).ContinueWith((continua) =>
+                    {
+                        c1.Image = c2.Image = null;
+                        c1.BackgroundImage = c2.BackgroundImage = Image.FromFile($"cartas/bloco.png");
+                        c1 = c2 = null;
+                    }, TaskScheduler.FromCurrentSynchronizationContext());
+                }
+            }
+
+        }
+
+        private void IniciarJogoDificil()
+        {
+            //limpa o espaço ocupado pelas cartas anteriores
+            LimparPictureBoxes();
+            SoundPlayer player = new SoundPlayer("audio/tema_dificil.wav");
+            player.Play();
+
+            int[] cartas = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8 };
+            Misturar(cartas);
+            jogoDificil = new int[4, 4];
+            int tamanhoCarta = 50;
+            int espaçamento = 60;
+            int margemEsquerda = 20;
+            int margemSuperior = 20;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.BackgroundImage = Image.FromFile($"cartas/bloco.png");
+                    pictureBox.BackgroundImageLayout = ImageLayout.Stretch;
+                    pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox.Width = pictureBox.Height = tamanhoCarta;
+                    pictureBox.Left = margemEsquerda + j * espaçamento;
+                    pictureBox.Top = margemSuperior + i * espaçamento;
+                    pictureBox.Click += PictureBox_Click;
+
+                    //coloca os valores embaralhados na picture box
+                    int index = i * 4 + j;
+                    pictureBox.Tag = cartas[index];
+                    Controls.Add(pictureBox);
+                    jogoDificil[i, j] = cartas[index];
+                }
+            }
+        }
+
+     
+
+        private void Reiniciar()
+        {
+            //reinicia o jogo
+            tentativas = iguais = 0;
+            VirarCartasParaBaixo();
+
+            gameover = false;
+
+            switch (modoAtual)
+            {
+                case ModoJogo.Normal:
+                    IniciarJogoNormal();
+                    break;
+                case ModoJogo.Dificil:
+                    IniciarJogoDificil();
+                    break;
+                default:
+                    break;
+            }
+
+            //reinicia o tempo
+            tentativas = 0;
+            txtPontos.Text = $"{tentativas}";
+            contagemRegressiva = tempoTotal;
+            timer1.Start();
+        }
+        private void VirarCartasParaBaixo()
+        {
+            //vira todas as cartas para baixo
+            foreach (Control control in Controls)
+            {
+                if (control is PictureBox pictureBox)
+                {
+                    pictureBox.Image = null;
+                    pictureBox.BackgroundImage = Image.FromFile("cartas/bloco.png");
+                }
+            }
+        }
+
+
+        private void Fim(String mensagem)
+        {
+            //final bom
+            SoundPlayer player = new SoundPlayer("audio/vitoria.wav");
+            player.Play();
+            timer1.Stop();
+            gameover = true;
+            MessageBox.Show($"{mensagem} Clique em Reiniciar para jogar novamente!", "Fim");
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reiniciar();
+        }
+
+        private void btnModoNormal_Click(object sender, EventArgs e)
+        {
+            modoAtual = ModoJogo.Normal;
+            Reiniciar();
+        }
+
+        private void btnModoDificil_Click(object sender, EventArgs e)
+        {
+            modoAtual = ModoJogo.Dificil;
+            Reiniciar();
+        }
+
+        private void Misturar(int[] array)
+        {
+            //embaralha as cartas
+            Random x = new Random();
+            int n = array.Length;
+            while (n > 0)
+            {
+                n--;
+                int y = x.Next(n + 1);
+                int temp = array[y];
+                array[y] = array[n];
+                array[n] = temp;
+            }
+        }
+
+        private void lblTempo_Click(object sender, EventArgs e)
+        {
+           //final ruim
+            contagemRegressiva--;
+            lblTempo.Text = $"Tempo Restante: {contagemRegressiva}";
+            if (contagemRegressiva == 0)
+            {  
+                timer1.Stop();
+                SoundPlayer player = new SoundPlayer("audio/perdeu.wav");
+                player.Play();
+                MessageBox.Show("Acabou o tempo.", "FIM");
+                
+                gameover = true;
+                
+            }
+        }
+
+        private void txtPontos_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPontos_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
